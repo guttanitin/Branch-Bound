278750e8bef0512e874fe5f905463b248f5bbbbb
Nitin Gutta <ngutta@umich.edu>
2018-05-30 23:03:49 -0400

    get rid of invalid moves needed to get result string

--- BranchandBound/src/BandB.java
+++ BranchandBound/src/BandB.java
@@ -4,7 +4,17 @@ import java.io.*;
 
 public class BandB {
 
-
+    public static boolean checkValidInput(String entry, String exit){
+        char[] entrystring = new char[entry.length()];
+        char[] exitstring = new char[exit.length()];
+        boolean valid = true;
+        for(int i = 0; i < entrystring.length; i++){
+            if(exit.indexOf(entrystring[i]) < i){
+                valid = false;
+            }
+        }
+        return valid;
+    }
 
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
@@ -18,19 +28,27 @@ public class BandB {
         startpattern = parts[0];
         finalpattern = parts[1];
         if(startpattern.length() < 3){
-            System.out.println("Invalid Entry");
+            System.out.println("Invalid Entry input has to be more than 2 letters");
         }
         else if (startpattern.equals(finalpattern)){
             System.out.print(startpattern + ' ' + finalpattern);
         }
         else {
-            inputpattern = new char[startpattern.length()];
-            for (int i = 0; i < inputpattern.length; i++) {
-                inputpattern[i] = startpattern.charAt(i);
+            boolean validentry = checkValidInput(startpattern,finalpattern);
+            if(!validentry){
+                System.out.println("Invalid Entry not a possible move");
             }
-            for (int i = 0; i < inputpattern.length; i++) {
-                System.out.print(inputpattern[i]);
+            else {
+                inputpattern = new char[startpattern.length()];
+                for (int i = 0; i < inputpattern.length; i++) {
+                    inputpattern[i] = startpattern.charAt(i);
+                }
+                for (int i = 0; i < inputpattern.length; i++) {
+                    System.out.print(inputpattern[i]);
+                }
+                System.out.println();
             }
+
         }
 
     }
