b3f9b52a67e0deeb9ade904a1efef916ba274e84
Nitin Gutta <ngutta@umich.edu>
2018-05-30 20:44:43 -0400

    split user input into 2 words and added functionality to check if length of string less than 3 then invalid and if the starting and final string are same then print out the same value.

--- BranchandBound/src/BandB.java
+++ BranchandBound/src/BandB.java
@@ -11,10 +11,27 @@ public class BandB {
         String input;
         String startpattern;
         String finalpattern;
-        System.out.print("Enter the starting word and final word: ");
-        input = scan.next();
-        startpattern = input.substring(0);
-        finalpattern = input.substring(1);
-        System.out.println("The words you entered are: " + startpattern + ' ' + finalpattern);
+        char [] inputpattern;
+        System.out.print("Enter the starting word and final word:");
+        input = scan.nextLine();
+        String[] parts = input.split("\\s+");
+        startpattern = parts[0];
+        finalpattern = parts[1];
+        if(startpattern.length() < 3){
+            System.out.println("Invalid Entry");
+        }
+        else if (startpattern.equals(finalpattern)){
+            System.out.print(startpattern + ' ' + finalpattern);
+        }
+        else {
+            inputpattern = new char[startpattern.length()];
+            for (int i = 0; i < inputpattern.length; i++) {
+                inputpattern[i] = startpattern.charAt(i);
+            }
+            for (int i = 0; i < inputpattern.length; i++) {
+                System.out.print(inputpattern[i]);
+            }
+        }
+
     }
 }
