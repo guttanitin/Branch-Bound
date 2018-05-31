a715e6ede9365c5a055989a34f746f20715cde80
Nitin Gutta <ngutta@umich.edu>
2018-05-30 20:13:18 -0400

    added input from user

--- BranchandBound/src/BandB.java
+++ BranchandBound/src/BandB.java
@@ -1,9 +1,20 @@
+
+import java.util.*;
+import java.io.*;
+
 public class BandB {
 
 
+
     public static void main(String [] args){
-
-
-
+        Scanner scan = new Scanner(System.in);
+        String input;
+        String startpattern;
+        String finalpattern;
+        System.out.print("Enter the starting word and final word: ");
+        input = scan.next();
+        startpattern = input.substring(0);
+        finalpattern = input.substring(1);
+        System.out.println("The words you entered are: " + startpattern + ' ' + finalpattern);
     }
 }
